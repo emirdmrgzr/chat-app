2847598ebb3b4754c1cf130214da938c7057c764
emirdmrgzr <emir.demirgezer@hotmail.com>
2023-04-01 16:26:39 +0300

    firebase linked and started to use

---
+++ src/firebase.js
@@ -0,0 +1,56 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { PasswordCheck } from "iconsax-react";
+import { ToastContainer, toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7BRwLNnQQX8OXHg--5HonuVsq7xBw2Qo",
+  authDomain: "auth-chat-d2b16.firebaseapp.com",
+  projectId: "auth-chat-d2b16",
+  storageBucket: "auth-chat-d2b16.appspot.com",
+  messagingSenderId: "321019873104",
+  appId: "1:321019873104:web:7f8badb206a53fec18ce55",
+  measurementId: "G-GL65BES06P"
+
+    // apiKey : process.env.API_KEY,
+    // authDomain: process.env.AUTH_DOMAIN,
+    // projectId : process.env.PROJECT_ID,
+    // storageBucket : process.env.STORAGE_BUCKET,
+    // messageSenderId : process.env.MESSAGE_SENDER_ID,
+    // appId : process.env.APP_ID,
+    // measurmentId : process.env.MEASURMENT_ID
+}
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export const register = async (email, password) =>{
+        try{
+        const {user} =  await createUserWithEmailAndPassword(auth, email, password);
+        return user;
+        }
+        catch(e){
+            console.log(e.message);
+        }
+}
+
+export const login = async (email,password) =>{
+    try{
+     const {user} =  await signInWithEmailAndPassword(auth, email, password);
+     return user;
+    } catch(e){
+        console.log(e.message);
+    }
+}
+
+export const logout = async() =>{
+    try{
+        await signOut(auth);
+        return true;
+    }
+    catch(e){
+        console.log(e.message);
+    }
+}
+
+export default app;

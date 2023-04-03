a9c55565030a6f04ca26de78b345603891c8c39a
emirdmrgzr <emir.demirgezer@hotmail.com>
2023-04-03 09:03:27 +0300

    fix bug about JSON.parse fixed

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,23 +1,58 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    user: JSON.parse(localStorage.getItem("user")) ?? false
-}
+const storedUser = localStorage.getItem("user");
 
-const auth = createSlice({
+let user = false;
+
+if (storedUser !== null) {
+  try {
+    user = JSON.parse(storedUser);
+  } catch (e) {
+    console.error("Invalid JSON in localStorage: ", storedUser);
+  }
+}
+const initialState = {
+    user: user
+};
+  
+  const authSlice = createSlice({
     name: "auth",
     initialState,
-    reducers:{
-        loginCheck: (state,action) =>{
-            localStorage.setItem("user", JSON.stringify(action.payload));
-            state.user = action.payload;
-        },
-        logoutCheck: state =>{
-            localStorage.removeItem("user");
-            state.user = false;
-        }
-    }
-})
+    reducers: {
+      loginCheck: (state, action) => {
+        state.user = action.payload;
+        localStorage.setItem("user", JSON.stringify(action.payload));
+      },
+      logoutCheck: (state) => {
+        state.user = null;
+        localStorage.removeItem("user");
+      },
+    },
+  });
+  
+  export const { loginCheck, logoutCheck } = authSlice.actions;
+  
+  export default authSlice.reducer;
 
-export const { loginCheck, logoutCheck} = auth.actions;
-export default auth.reducer;
+
+// const initialState = {
+//     user: JSON.parse(localStorage.getItem("user")) ?? false
+// }
+
+// const auth = createSlice({
+//     name: "auth",
+//     initialState,
+//     reducers:{
+//         loginCheck: (state,action) =>{
+//             localStorage.setItem("user", JSON.stringify(action.payload));
+//             state.user = action.payload;
+//         },
+//         logoutCheck: state =>{
+//             localStorage.removeItem("user");
+//             state.user = null;
+//         }
+//     }
+// })
+
+// export const { loginCheck, logoutCheck} = auth.actions;
+// export default auth.reducer;

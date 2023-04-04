e4f3e1eb02a7b8d2512e53f9b4f33404bf0f822c
emirdmrgzr <emir.demirgezer@hotmail.com>
2023-04-04 06:51:02 +0300

    Sidebar changed to more useful version

--- src/components/LeftBarComponent.js
+++ src/components/LeftBarComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Message, Home2, SearchNormal1, Calendar2, Setting2} from 'iconsax-react';
+import { Message, Home2, SearchNormal1, Calendar2, Setting2, ProfileCircle} from 'iconsax-react';
 import { Link } from 'react-router-dom';
 
 const LeftBarComponent = () => {
@@ -7,12 +7,12 @@ const LeftBarComponent = () => {
     <div className='sticky leftbar-component bg-gray-100 w-full h-full shadow-leftbar z-10'>
         <div className='leftbar-elements w-full h-98 flex flex-col items-center mt-3'>
           <div className='w-12 h-12 bg-indigo-400 rounded-xl flex justify-center items-center'>
-            <Link to="/user-profile" className='text-3xl font-semibold text-gray-200 cursor-pointer'>C</Link>
+            <Link to="/" className='text-3xl font-semibold text-gray-200 cursor-pointer'>C</Link>
           </div>
           <div className='leftbar-importants mt-11'>
           <Link to="/" ><Home2 className="cursor-pointer" size="32" color="black"/></Link>
+          <Link to="user-profile"><ProfileCircle className='mt-7' size="32" color="black"/></Link>
           <Message className="mt-7 cursor-pointer" size="32" color="black"/>
-          <SearchNormal1 className="mt-7 cursor-pointer" size="32" color="black"/>
           <Calendar2 className="mt-7 cursor-pointer" size="32" color="black"/>
           </div>
           <div className='leftbar-settings mt-auto mb-8'>

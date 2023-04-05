2f0202d948d0f42c61a5520c86af1b6b2c9ab688
emirdmrgzr <emir.demirgezer@hotmail.com>
2023-04-05 15:25:29 +0300

    Routes in App is updated

--- src/components/MyRouter.js
+++ src/components/MyRouter.js
@@ -2,14 +2,14 @@ import NormalPage from "../pages/NormalPage";
 import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
-import UserProfileEdit from "./UserProfileEdit";
+import UserProfile from "./UserProfile";
 const MyRouter = () =>{
     return(
       <Routes>
         <Route path="/" element={<NormalPage />} />
         <Route path="/login" element={<Login/>} />
-        <Route path="signup" element={<SignUp/>}/>
-        <Route path="user-profile" element={<UserProfileEdit/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/user-profile" element={<UserProfile/>}/>
       </Routes>
     )
 }

561e62d5aa12f4b3a168b72a71d5b5c7e8ed1915
emirdmrgzr <emir.demirgezer@hotmail.com>
2023-04-16 15:39:06 +0300

    feat: username added and firestore connected

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,20 +3,31 @@ import { register } from '../firebase';
 import { Toaster } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
+import {collection, doc, setDoc, addDoc} from "firebase/firestore";
+import { db, storage } from '../firebase';
 
 const SignUp = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const navigate = useNavigate();
   const handleSubmit = async (e) =>{
     e.preventDefault();
+    try{
     const user = await register(email,password);
-    console.log(user);
-    toast.success("Signed up Successfully")
-    navigate("/",{
-      replace: true
+     user.displayName = name;
+     await navigate("/");
+     await setDoc(doc(db, "users", user.uid), {
+      uid: user.uid,
+      email,
+      name,
     });
+    await setDoc(doc(db, "userChats", user.uid), {}); 
+    
+    }
+    catch(e){
+      console.log(e);
+    }
   }
   
   return (
@@ -25,6 +36,16 @@ const SignUp = () => {
       <div className='bg-gray-100 p-16 rounded-2xl shadow-lg'>
       <p className="mb-8 text-center font-medium text-3xl text-indigo-400 ">SIGN UP</p>
     <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+    <div className="md:flex md:items-center mb-6">
+      <div className="md:w-1/3">
+        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" >
+          Name
+        </label>
+      </div>
+      <div className="md:w-2/3">
+        <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" onChange={e=>setName(e.target.value)} type="text" value={name} placeholder="John Doe"/>
+      </div>
+    </div>
     <div className="md:flex md:items-center mb-6">
       <div className="md:w-1/3">
         <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" >
@@ -61,7 +82,7 @@ const SignUp = () => {
   </div>
   
 
-   
+  
   )
 } 
 
